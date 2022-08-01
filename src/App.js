02fa57e78e31f9c8016bbb7eb7cf7c417b375dfb
Salman Awaisa <98347301+salmanowes2022@users.noreply.github.com>
2022-08-02 00:09:23 +0400

    portfolio using react

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import NavBar from './components/navbar/NavBar'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+
+
+const App = () => {
+  return (
+    < > 
+    <Header/>
+    <About/>
+    <Contact/>
+    <Experience/>
+    <Footer/>
+    <NavBar/>
+    <Portfolio/>
+    <Testimonials/>
+
+
+    
+
+    
+
+
+
+
+    </>
+  )
+}
+
+export default App
